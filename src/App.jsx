251778d18f17b2233605681297035cf12a503f42
Nguyễn Tiến Hoàng <hoangleo1303@gmail.com>
2024-05-29 02:05:45 +0700

    asd;lf

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  let initNumber = 0;
+
+  return (
+    <>
+    <div className="heart"></div>
+    <h1>Kiow lov Miaooooooooo</h1>
+    <div>
+      <div id="screen">
+
+      </div>
+      <div id="buttons">
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
